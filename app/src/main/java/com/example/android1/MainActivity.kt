6ef1f300362a32d65349e3febf1b160a33e83b75
Liraz <liraz581cohen@gmail.com>
2025-01-23 21:00:16 +0200

    (MainActivity.kt) : added button functionality

--- app/src/main/java/com/example/android1/MainActivity.kt
+++ app/src/main/java/com/example/android1/MainActivity.kt
@@ -1,10 +1,23 @@
 package com.example.android1
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
+var nextToPlay: String = "X"
+var gameEnded: Boolean = false
+var status: String = if (gameEnded) "Winner" else "$nextToPlay To Play"
+
+val buttonIds = listOf(
+    R.id.button1, R.id.button2, R.id.button3,
+    R.id.button4, R.id.button5, R.id.button6,
+    R.id.button7, R.id.button8, R.id.button9
+)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,10 +25,32 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val gameStatus: TextView = findViewById(R.id.gameStatus)
+        gameStatus.text = "$nextToPlay To Play"
+
+        for (btnId in buttonIds) {
+            findViewById<Button>(btnId).setOnClickListener {
+                onButtonClick(it)
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    private fun onButtonClick(view: View?) {
+        val button = view as Button
+
+        if (button.text === "") {
+            button.text = nextToPlay
+            nextToPlay = if (nextToPlay === "X") "O" else "X"
+
+            val gameStatus: TextView = findViewById(R.id.gameStatus)
+            status = "$nextToPlay To Play"
+            gameStatus.text = status
         }
     }
 }
